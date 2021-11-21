2cd9011044723d823ab9d6cbdb41ac6555002373
Pedro Bittencourt <pbittenc@gmail.com>
2021-11-21 10:40:38 -0300

    implementado cache decorator no dom injection

--- app/src/decorators/dom-injector.ts
+++ app/src/decorators/dom-injector.ts
@@ -4,9 +4,12 @@ export function domInjector(seletor: string) {
         propertyKey: string
     ) {
         console.log(`Modificando prototype ${target.constructor.name} e adicionando getter para a propriedade ${propertyKey}`)
+        let elemento: HTMLElement
         const getter = function () {
-            const elemento = document.querySelector(seletor)
-            console.log(`Buscando elemento do DOM com o seletor ${seletor} para injetar em ${propertyKey}`)
+            if (!elemento){
+                elemento = <HTMLElement>document.querySelector(seletor)
+                console.log(`Buscando elemento do DOM com o seletor ${seletor} para injetar em ${propertyKey}`)
+            }
             return elemento
         }
 
