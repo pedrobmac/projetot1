2b721c5e6bc1dc9920413bb2bc8604a5e2af0aed
Pedro Bittencourt <pbittenc@gmail.com>
2021-11-21 17:53:10 -0300

    criacao de modelo para estender todas interfaces

--- app/dist/js/models/negociacao.js
+++ app/dist/js/models/negociacao.js
@@ -24,4 +24,9 @@ export class Negociacao {
         Quantidade: ${this.quantidade},
         Valor: ${this.valor}`;
     }
+    ehIgual(negociacao) {
+        return this.data.getDate() === negociacao.data.getDate()
+            && this.data.getMonth() === negociacao.data.getMonth()
+            && this.data.getFullYear() === negociacao.data.getFullYear();
+    }
 }
--- app/src/models/negociacoes.ts
+++ app/src/models/negociacoes.ts
@@ -1,7 +1,8 @@
-import { Imprimivel } from "../utils/imprimivel.js";
+import { Modelo } from "../interfaces/modelo.js";
 import { Negociacao } from "./negociacao.js";
 
-export class Negociacoes implements Imprimivel{
+export class Negociacoes implements Modelo<Negociacoes>{
+
     // private negociacoes: Array<Negociacao> = []
     private negociacoes: Negociacao[] = []
 
@@ -16,5 +17,9 @@ export class Negociacoes implements Imprimivel{
 
     public paraTexto(): string {
         return JSON.stringify(this.negociacoes, null, 2)
+    }
+
+    public ehIgual(negociacoes: Negociacoes): boolean {
+        return JSON.stringify(this.negociacoes) === JSON.stringify(negociacoes.lista)
     }
 }
